f63683f8e0bb2b3665d87d470e7aa45f038b7c46
Fabian Eulitz <dev@sustineo.de>
2022-08-23 22:14:57 +0200

    Added GitLab pages
    Changed poetry version
    
    Added needs

--- docs/export_openapi.py
+++ docs/export_openapi.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # Local modules
 from sureflap_api.main import app
 
-OPENAPI_PATH = Path(__file__).resolve().parent / "openapi.json"
+OPENAPI_PATH = Path(__file__).resolve().parent.parent / "public" / "openapi.json"
 
 with open(OPENAPI_PATH, "w") as f:
     f.write(json.dumps(app.openapi()))
